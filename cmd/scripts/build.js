dc56c905b4164ba822058e2e045edede55a2baee
Shi Chen <chenshi@microsoft.com>
2021-07-13 15:05:22 +0800

    chore: fix vulnerabilities and remove gulp (#83)
    * fix vulnerabilities and remove gulp
    
    * add copyright

---
+++ cmd/scripts/build.js
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+function isWin() {
+	return /^win/.test(process.platform);
+}
+
+function mvnw() {
+	return isWin() ? 'mvnw.cmd' : './mvnw';
+}
+
+const cp = require('child_process');
+const path = require('path');
+const fse = require('fs-extra');
+const glob = require('glob');
+const rimraf = require('rimraf');
+const rootPath = path.join(__dirname, '..', '..');
+
+rimraf.sync(path.join(__dirname, 'repository'));
+rimraf.sync(path.join(__dirname, 'workspace'));
+const tgzs = glob.sync('*.tgz');
+for (const tgz of tgzs) {
+	fse.removeSync(tgz);
+}
+
+cp.execSync(`${mvnw()} clean verify`, { cwd: rootPath, stdio: [0, 1, 2] });
+const sourceRepositoryPath = path.join(rootPath, 'com.microsoft.java.lsif.product', 'target', 'repository');
+const targetRepositoryPath = path.join(rootPath, 'cmd', 'repository');
+
+fse.copySync(path.join(sourceRepositoryPath, 'config_linux'), path.join(targetRepositoryPath, 'config_linux'));
+fse.copySync(path.join(sourceRepositoryPath, 'config_mac'), path.join(targetRepositoryPath, 'config_mac'));
+fse.copySync(path.join(sourceRepositoryPath, 'config_win'), path.join(targetRepositoryPath, 'config_win'));
+fse.copySync(path.join(sourceRepositoryPath, 'features'), path.join(targetRepositoryPath, 'features'));
+fse.copySync(path.join(sourceRepositoryPath, 'plugins'), path.join(targetRepositoryPath, 'plugins'));
